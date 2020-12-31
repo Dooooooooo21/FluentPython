8f6e8e003545e275e219ec4a50ff54a69717c05e
dly <1083404373@qq.com>
2020-12-31 13:45:37 +0800

    magic method

---
+++ ch01/PythonCard.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+# @Time    : 2020/12/31 13:27
+# @Author  : dly
+# @File    : PythonCard.py
+# @Desc    :
+
+import collections
+from random import choices, choice
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+
+class FrenchDeck:
+    # 13 张牌
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    # 四种花色
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        # 生成所有牌
+        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, item):
+        return self._cards[item]
+
+
+deck = FrenchDeck()
+# 实际调用 deck.__len__()
+print(len(deck))
+print(deck.__len__())
+
+# 实际调用 deck.__getitem__(0)
+print(deck[0])
+print(deck.__getitem__(0))
+
+# 随机抽牌
+print(choice(deck))
+print(choices(deck, k=5))
---
+++ ch01/第一章 Python数据模型.md
@@ -0,0 +1,15 @@
+#### 第一章 Python数据模型
+
+
+
+魔术方法(magic method): 
+
+```python
+# 方法名规则
+__name__
+
+# my_collection[key] 解释器实际会调用 my_collection.__getitem__(key)
+```
+
+
+
